4d1889734e00cea7280f89c70700d7f08c0e7c65
Jesica K <jesicak@MacBook-Pro.local>
2020-09-12 15:17:01 +0530

    Lec-26 Property Binding

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import {ServersComponent} from './servers/servers.component';
+import {WarningComponent} from './warning/warning.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector:'app-warning',
+    templateUrl: './warning.component.html',
+    styles:[`
+        h3{
+            color: 'green'
+        }
+    `]
+})
+export class WarningComponent{
+
+}
